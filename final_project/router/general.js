abd40c30db7000d6f800e21cdc1d8009e8954bfa
Ooguztiras <oguztiras3@gmail.com>
2025-05-18 09:11:08 -0400

    done third task

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,8 +23,14 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    let theBook = {}
+    Object.values(books).forEach((book) => {
+        if (book.author === author) {
+            theBook = book;
+        }
+    });
+    res.send(JSON.stringify(theBook));
 });
 
 // Get all books based on title
